646efd2535b913049997aa661368536918a304de
sainathsr <sainathsr95@gmail.com>
2021-01-03 00:09:34 +0530

    Add downward scroll

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <InfiniteScroll class="infi-scroll"> </InfiniteScroll>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import InfiniteScroll from "./components/InfiniteScroll";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    InfiniteScroll,
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+
+  .infi-scroll {
+    margin: 60px;
+    height: 100%;
+  }
 }
 </style>
--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,12 +1,130 @@
 <template>
-  <div class="scroll-container">
-    <div class=""></div>
+  <div class="infi-scroll-comp-root">
+    <div class="scroll-container" ref="scrollContainer" v-if="!initialLoad">
+      <div class="list-container" ref="listContainer">
+        <div class="list-item" v-for="(item, index) in list" :key="index">
+          {{ item.name }}
+        </div>
+      </div>
+      <div class="loadingMore">LOADING .....</div>
+      <div class="sentinel" ref="sentinel"></div>
+    </div>
+    <div class="initialLoad" v-else>INITIAL LIST LOADING .....</div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  created() {
+    //load first page
+    this.fetchItemsAPI(0, this.pageCount).then((items) => {
+      this.list.push(...items);
+      this.pageNumber++;
+      this.initialLoad = false;
+      // this.setUpInterSectionObserver();
+    });
+  },
+  destroyed() {
+    if (this.listEndObserver) {
+      this.listEndObserver.disconnect();
+    }
+  },
+  mounted() {},
+  data() {
+    return {
+      list: [],
+      /**
+       * if we extracted infinite-scroll to a generic component,
+       * it would need just these props.loadingMore,canLoadMore and the list
+       */
+
+      loadingMore: false,
+      canLoadMore: true, //list has to end at some point
+
+      //extra stuff
+      initialLoad: true, //initial load state ,if you want  to use a different BIGGER loading animation  etc
+      pageNumber: 0, //canLoadMore is set to false when last page is reached
+      pageCount: 10,
+    };
+  },
+  methods: {
+    setUpInterSectionObserver() {
+      let options = {
+        root: this.$refs["scrollContainer"],
+        margin: "10px",
+      };
+      this.listEndObserver = new IntersectionObserver(
+        this.handleIntersection,
+        options
+      );
+
+      this.listEndObserver.observe(this.$refs["sentinel"]);
+    },
+    handleIntersection() {
+      if (this.canLoadMore) {
+        this.loadMore();
+      }
+    },
+    async loadMore() {
+      try {
+        this.isLoadingMore = true;
+        let items = this.fetchItemsAPI(this.pageNumber, this.pageCount);
+        this.pageNumber++;
+        this.list.push(...items);
+      } catch (error) {
+        console.log("Reached end of page", error);
+        this.canLoadMore = false;
+      } finally {
+        this.isLoadingMore = false;
+      }
+    },
+
+    fetchItemsAPI(pageNumber, pageCount) {
+      //assume 10 items per page
+
+      return new Promise((res, rej) => {
+        let data = [];
+        for (let i = pageNumber * 10; i < pageNumber * 10 + 10; i++) {
+          data.push({
+            name: "Item " + i,
+          });
+        }
+
+        setTimeout(() => {
+          if (pageNumber < pageCount) {
+            res(data);
+          } else {
+            rej(new Error("No more items to load"));
+          }
+        }, 2000);
+      });
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.scroll-container {
+  height: 100%;
+  overflow-y: scroll;
+}
+.sentinel {
+  height: 0px;
+}
+
+.list-container {
+  border: 1px solid darkgray;
+}
+.list-item {
+  margin-bottom: 10px;
+  border: 1px solid darkgoldenrod;
+  background: lightgoldenrodyellow;
+  color: black;
+  padding: 25px;
+  line-height: 25px;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 18px;
+  border-radius: 5px;
+}
 </style>
